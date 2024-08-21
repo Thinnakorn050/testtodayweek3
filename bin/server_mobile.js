460303bd7335ec752cec28cfdb1d99d14b00d7e5
Thinnakorn050 <165901728+Thinnakorn050@users.noreply.github.com>
2024-08-21 12:04:12 +0700

    push

--- bin/server_mobile.js
+++ bin/server_mobile.js
@@ -1,15 +1,68 @@
-// //-----------Exercise 3: Get a Node.js JSON array.----------------//
+// //---------------Test today------------------//
+// const con = require('./db');
+// const express = require('express');
+// const bcrypt = require('bcrypt');
+// const app = express();
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// // password generator
+// app.get('/password/:pass', (req, res) => {
+//     const password = req.params.pass;
+//     bcrypt.hash(password, 10, function(err, hash) {
+//         if(err) {
+//             return res.status(500).send('Hashing error');
+//         }
+//         res.send(hash);
+//     });
+// });
+
+
+// // login
+// app.post('/login', (req, res) => {
+//     const {username, password} = req.body;
+//     const sql = "SELECT id, password FROM users WHERE username = ?";
+//     con.query(sql, [username], function(err, results) {
+//         if(err) {
+//             return res.status(500).send("Database server error");
+//         }
+//         if(results.length != 1) {
+//             return res.status(401).send("Wrong username");
+//         }
+//         // compare passwords
+//         bcrypt.compare(password, results[0].password, function(err, same) {
+//             if(err) {
+//                 return res.status(500).send("Hashing error");
+//             }
+//             if(same) {
+//                 return res.send("Login OK");
+//             }
+//             return res.status(401).send("Wrong password");
+//         });
+//     })
+// });
+
+
+// // ---------- Server starts here ---------
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log('Server is running at ' + PORT);
+// });
+
+
+
+
 const con = require('./db');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const app = express();
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// password generator
+// Password generator
 app.get('/password/:pass', (req, res) => {
     const password = req.params.pass;
     bcrypt.hash(password, 10, function(err, hash) {
@@ -20,8 +73,7 @@ app.get('/password/:pass', (req, res) => {
     });
 });
 
-
-// login
+// Login
 app.post('/login', (req, res) => {
     const {username, password} = req.body;
     const sql = "SELECT id, password FROM users WHERE username = ?";
@@ -32,7 +84,7 @@ app.post('/login', (req, res) => {
         if(results.length != 1) {
             return res.status(401).send("Wrong username");
         }
-        // compare passwords
+        // Compare passwords
         bcrypt.compare(password, results[0].password, function(err, same) {
             if(err) {
                 return res.status(500).send("Hashing error");
@@ -42,14 +94,80 @@ app.post('/login', (req, res) => {
             }
             return res.status(401).send("Wrong password");
         });
-    })
+    });
+});
+
+// Get all expenses
+app.get('/expenses', (req, res) => {
+    const sql = "SELECT * FROM expense";
+    con.query(sql, (err, results) => {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    });
+});
+
+// Get today's expenses
+app.get('/expenses/today', (req, res) => {
+    const today = new Date().toISOString().split('T')[0]; // Get YYYY-MM-DD format
+    const sql = "SELECT * FROM expense WHERE DATE(date) = ?";
+    con.query(sql, [today], (err, results) => {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    });
+});
+
+// Search expenses by name
+app.get('/expenses/search', (req, res) => {
+    const keyword = `%${req.query.keyword}%`;
+    const sql = "SELECT * FROM expense WHERE item LIKE ?";
+    con.query(sql, [keyword], (err, results) => {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        res.json(results);
+    });
 });
 
 
-// ---------- Server starts here ---------
-const PORT = 3000;
+// Add new expense
+app.post('/expenses', (req, res) => {
+    const { item, amount, date } = req.body;  
+    console.log("Received:", item, amount, date); 
+    const sql = "INSERT INTO expense (item, paid, date) VALUES (?, ?, ?)";
+    con.query(sql, [item, amount, date], (err, results) => {
+        if(err) {
+            console.error("Database error:", err);  
+            return res.status(500).send("Database server error");
+        }
+        console.log("Results:", results); 
+        res.status(200).send("Expense added successfully");
+    });
+});
+
+
+
+
+// Delete expense
+app.delete('/expenses/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = "DELETE FROM expense WHERE id = ?";
+    con.query(sql, [id], (err, results) => {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        if (results.affectedRows === 0) {
+            return res.status(404).send("Expense not found");
+        }
+        res.status(200).send("Expense deleted successfully");
+    });
+});
+
+// Server starts here
+const PORT = 4000;
 app.listen(PORT, () => {
     console.log('Server is running at ' + PORT);
 });
-
-
--- expense.sql
+++ expense.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2024 at 10:49 AM
+-- Generation Time: Aug 21, 2024 at 07:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -40,30 +40,8 @@ CREATE TABLE `expense` (
 --
 
 INSERT INTO `expense` (`id`, `user_id`, `item`, `paid`, `date`) VALUES
-(1, 1, 'lunch', 70, '2024-08-09 12:07:33'),
-(2, 1, 'coffee', 45, '2024-08-09 13:07:33'),
-(3, 1, 'rent', 1600, '2024-08-01 07:26:53'),
-(4, 2, 'lunch', 50, '2024-08-09 13:27:39');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'Lisa', '$2b$10$9uOv7acsMSHG.nNH1UstD.V7U7WtUoQs5Hph0DVeYzOSkgt9oOI.m'),
-(2, 'Tom', '$2b$10$dzdavGYECltbm/OrzLaafebqp3Zglb2r/itzCP7veyqZkkDN1iSnK');
+(4, 2, 'bun', 50, '2024-08-09 21:02:36'),
+(9, 2, 'lunch', 50, '2024-08-21 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -77,12 +55,6 @@ ALTER TABLE `expense`
   ADD KEY `fk_expense_user` (`user_id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -90,13 +62,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `expense`
 --
 ALTER TABLE `expense`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
